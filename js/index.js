4abf5aaba60fca483c08fe5ec0b29adf500caf14
Brian Abeyta-Pratt <babeytapratt@gmail.com>
2020-09-29 16:55:56 -0600

    first commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+const funBus = document.querySelector('.logo-heading')
+const homeNav = document.querySelector('.nav-link')
+const welcomeToFunBus = document.querySelector('.intro.h2')
+const welcomeParagraph = document.querySelector('.intro.p')
+const letsGo = document.querySelector('.content-section.text-content.h2')
+const adventureAwaits = document.querySelector('.content-section inner-content.text-content.h2')
+const pickYourDestination = document.querySelector('.content-destination.h2')
+const funInTheSun = document.querySelector('.destination.h4')
+const mountainExcursion = document.querySelector('.destination.p')
+const signMeUp = document.querySelector('.btn')
+
+
+
+
+funBus.addEventListener('mouseover', (event) => {
+    funBus.classList.backgroundColor = blue;
+})

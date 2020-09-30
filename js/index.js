b7c2341cbc4b5ad388f717be5587e69b23e759f4
Brian Abeyta-Pratt <babeytapratt@gmail.com>
2020-09-29 21:16:38 -0600

    finished

--- js/index.js
+++ js/index.js
@@ -1,18 +1,107 @@
 // Your code goes here
 const funBus = document.querySelector('.logo-heading')
-const homeNav = document.querySelector('.nav-link')
-const welcomeToFunBus = document.querySelector('.intro.h2')
-const welcomeParagraph = document.querySelector('.intro.p')
-const letsGo = document.querySelector('.content-section.text-content.h2')
-const adventureAwaits = document.querySelector('.content-section inner-content.text-content.h2')
-const pickYourDestination = document.querySelector('.content-destination.h2')
-const funInTheSun = document.querySelector('.destination.h4')
-const mountainExcursion = document.querySelector('.destination.p')
-const signMeUp = document.querySelector('.btn')
+const homeNav = document.querySelector('a')
+const welcomeToFunBus = document.querySelector('.intro h2')
+const welcomeParagraph = document.querySelector('.intro p')
+const letsGo = document.querySelector('.content-section .text-content h2')
+const letsGoPara = document.querySelector('.content-section .text-content p')
+const adventureAwaits = document.querySelector('.inverse-content .text-content h2')
+const adventureAwaitsPara = document.querySelector('.inverse-content .text-content p')
+const pickYourDestination = document.querySelector('.content-destination h2')
+const pickYourDestinationPara = document.querySelector('.content-destination p')
+const funInTheSun = document.querySelector('.destination h4')
 
 
 
 
 funBus.addEventListener('mouseover', (event) => {
-    funBus.classList.backgroundColor = blue;
-})
+    event.target.style.backgroundColor = "blue";
+    })
+
+funBus.addEventListener('mouseleave', (event) => {
+    event.target.style.backgroundColor = "white";
+    })
+
+homeNav.addEventListener('mouseenter', (event) => {
+    event.target.style.backgroundColor = "royalpurple";
+    })
+
+welcomeToFunBus.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = "yellow";
+    })
+
+welcomeToFunBus.addEventListener('mouseleave', (event) => {
+    event.target.style.backgroundColor = "white";
+    })
+
+welcomeParagraph.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = "red";
+    })
+
+welcomeParagraph.addEventListener('mouseleave', (event) => {
+    event.target.style.backgroundColor = "white";
+    })
+
+letsGo.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = "lavender";
+    })
+
+letsGo.addEventListener('mouseleave', (event) => {
+    event.target.style.backgroundColor = "white";
+    })
+
+adventureAwaits.addEventListener('mouseover', (event)=> {
+    event.target.style.backgroundColor = "orange";
+    })
+
+adventureAwaits.addEventListener('mouseleave', (event)=> {
+    event.target.style.backgroundColor = "white";
+    })
+
+letsGoPara.addEventListener('mouseover', (event)=> {
+    event.target.style.backgroundColor = "turquoise";
+    })
+
+letsGoPara.addEventListener('mouseleave', (event)=> {
+    event.target.style.backgroundColor = "white";
+    })
+
+adventureAwaits.addEventListener('mouseover', (event)=> {
+    event.target.style.backgroundColor = "orange";
+    })
+
+adventureAwaits.addEventListener('mouseleave', (event)=> {
+    event.target.style.backgroundColor = "white";
+    })
+
+adventureAwaitsPara.addEventListener('mouseover', (event)=> {
+    event.target.style.backgroundColor = "pink";
+    })
+
+adventureAwaitsPara.addEventListener('mouseleave', (event)=> {
+    event.target.style.backgroundColor = "white";
+    })
+
+pickYourDestination.addEventListener('mouseover', (event)=> {
+    event.target.style.backgroundColor = "green";
+    })
+
+pickYourDestination.addEventListener('mouseleave', (event)=> {
+    event.target.style.backgroundColor = "white";
+    })
+
+pickYourDestinationPara.addEventListener('mouseover', (event)=> {
+    event.target.style.backgroundColor = "skyblue";
+    })
+
+pickYourDestinationPara.addEventListener('mouseleave', (event)=> {
+    event.target.style.backgroundColor = "white";
+    })
+
+funInTheSun.addEventListener('mouseover', (event)=> {
+    event.target.style.backgroundColor = "teal";
+    })
+
+funInTheSun.addEventListener('mouseleave', (event)=> {
+    event.target.style.backgroundColor = "white";
+    })
